2c7da4325c6c7394f7ebee0f3850bcd17f409e6d
LamSB293 <lamnvhe181525@fpt.edu.vn>
2024-11-11 00:35:57 +0700

    Init

--- Library/obj/Debug/net8.0-windows/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0-windows/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3f6ce602a0a38c295034a141bea269163c2a1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
